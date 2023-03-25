8f6f9deb5bed01c979e3167d32ef0b95849f3dfb
iamando <iamando@tuta.io>
2023-03-25 16:59:56 +0300

    feature: use boot on index

--- index.js
+++ index.js
@@ -1,12 +1,5 @@
 #!/usr/bin/env node
 
-/**
- * terb
- * Web interface for managing terminal
- *
- * @author iamando <iamando@tuta.io>
- */
-
 const init = require('./utils/init')
 const cli = require('./utils/cli')
 const log = require('./utils/log')
@@ -15,9 +8,11 @@ const input = cli.input
 const flags = cli.flags
 const { clear, debug } = flags
 
-;(async () => {
+const boot = async () => {
   init({ clear })
   input.includes('help') && cli.showHelp(0)
 
   debug && log(flags)
-})()
+}
+
+boot()
