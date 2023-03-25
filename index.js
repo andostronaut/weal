948e11ddc3b325f791d165398caae406bf847072
iamando <iamando@tuta.io>
2023-03-25 19:25:14 +0300

    feature: setup open func on cli

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const init = require('./lib/utils/init')
 const cli = require('./lib/utils/cli')
 const log = require('./lib/utils/log')
 
+const terb = require('./lib/terb')
+
 const input = cli.input
 const flags = cli.flags
 const { clear, debug } = flags
@@ -11,6 +13,7 @@ const { clear, debug } = flags
 const boot = async () => {
   init({ clear })
   input.includes('help') && cli.showHelp(0)
+  input.includes('open') && terb.open()
 
   debug && log(flags)
 }
