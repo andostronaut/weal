d47dfd8a727fd17c749fa21416d66904f320800e
iamando <iamando@tuta.io>
2023-03-26 14:58:51 +0300

    feature: fix terminal ui on default

--- interfaces/utils/terminal.js
+++ interfaces/utils/terminal.js
@@ -1,19 +1,15 @@
 const React = require('react')
-// const Terminal = require('react-terminal-ui')
+const Terminal = require('react-terminal-ui')
 
 const TerminalUI = () => {
   return (
-    <div className='container'>
-      {/* <Terminal
-        name="Terb"
-        colorMode={Terminal.ColorMode.Light}
-        onInput={(terminalInput) => console.log(`New terminal input received: '${terminalInput}'`)}
-      >
-        <Terminal.TerminalOutput>Welcome to terb interface</Terminal.TerminalOutput>
-      </Terminal> */}
-
-      <h1>Hello from Terb</h1>
-    </div>
+    <Terminal.default
+      name='Terb'
+      colorMode={Terminal.ColorMode.Dark}
+      onInput={(terminalInput) => console.log(`New terminal input received: '${terminalInput}'`)}
+    >
+      <Terminal.TerminalOutput>Welcome to terb interface</Terminal.TerminalOutput>
+    </Terminal.default>
   )
 }
 
