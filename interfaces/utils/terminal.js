038e50c8d21ad92983e163b4d0f1207115794ec8
iamando <iamando@tuta.io>
2023-03-26 16:12:55 +0300

    feature: use history on terminal for dont loose it

--- interfaces/utils/terminal.js
+++ interfaces/utils/terminal.js
@@ -2,17 +2,45 @@ const React = require('react')
 const Terminal = require('react-terminal-ui')
 
 const TerminalComponent = Terminal.default
+const TerminalInput = Terminal.TerminalInput
 const TerminalOutput = Terminal.TerminalOutput
 const ColorMode = Terminal.ColorMode
 
 const TerminalUI = () => {
+  const [mode, setMode] = React.useState('light')
+  const [history, setHistory] = React.useState([
+    <TerminalOutput key='welcome'>Welcome to terb interface</TerminalOutput>,
+    <TerminalOutput key='empty'></TerminalOutput>,
+    <TerminalOutput key='commands'>The following example commands are provided:</TerminalOutput>,
+    <TerminalOutput key='mode'>'mode' will change the terminal theme.</TerminalOutput>,
+    <TerminalOutput key='clear'>'clear' will clear the terminal.</TerminalOutput>
+  ])
+
+  const themeByMode = {
+    light: ColorMode.Light,
+    dark: ColorMode.Dark
+  }
+
+  const handleInput = (terminalInput) => {
+    let inputs = [...lineData]
+    inputs.push(<TerminalInput>{terminalInput}</TerminalInput>)
+
+    if (terminalInput.toLocaleLowerCase().trim() === 'mode=light') {
+      setMode('light')
+    } else if (terminalInput.toLocaleLowerCase().trim() === 'mode=dark') {
+      setMode('dark')
+    } else if (terminalInput.toLocaleLowerCase().trim() === 'clear') {
+      ld = []
+    } else if (terminalInput) {
+      ld.push(<TerminalOutput>Unrecognized command</TerminalOutput>)
+    }
+
+    setHistory(inputs)
+  }
+
   return (
-    <TerminalComponent
-      name='Terb'
-      colorMode={ColorMode.Dark}
-      onInput={(terminalInput) => console.log(`New terminal input received: '${terminalInput}'`)}
-    >
-      <TerminalOutput>Welcome to terb interface</TerminalOutput>
+    <TerminalComponent name='Terb' colorMode={themeByMode[mode]} onInput={handleInput}>
+      {history}
     </TerminalComponent>
   )
 }
