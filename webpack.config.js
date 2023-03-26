978fd97cafa4a209571a40757ec5daf8e955e721
iamando <iamando@tuta.io>
2023-03-26 10:07:32 +0300

    feature: setup react interfaces on webpack & babel config and serve

---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+const path = require('path')
+const HtmlWebPackPlugin = require('html-webpack-plugin')
+
+const htmlPlugin = new HtmlWebPackPlugin({
+  template: './interfaces/index.html',
+  filename: './index.html'
+})
+
+module.exports = {
+  mode: 'development',
+  entry: './interfaces/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  resolve: {
+    alias: {
+      src: path.resolve(__dirname, 'interfaces')
+    }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader'
+      },
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader'
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      }
+    ]
+  },
+  plugins: [htmlPlugin]
+}
