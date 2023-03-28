a228c3350f637b2050be084bf15827854d537dca
Ando RAMANAMIHANTA <iamando@tuta.io>
2023-03-28 10:26:43 +0300

    chore: re-struct lib cli to new weal struct

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const init = require('./lib/utils/init')
 const cli = require('./lib/utils/cli')
 const log = require('./lib/utils/log')
 
-const terb = require('./lib/terb')
+const weal = require('./lib/weal')
 
 const input = cli.input
 const flags = cli.flags
@@ -13,7 +13,7 @@ const { clear, debug } = flags
 const boot = async () => {
   init({ clear })
   input.includes('help') && cli.showHelp(0)
-  input.includes('open') && terb.open()
+  input.includes('open') && weal.open()
 
   debug && log(flags)
 }
