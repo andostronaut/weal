2a056cb3e24a6e291c4019cf0574d3c172709a28
Ando RAMANAMIHANTA <iamando@tuta.io>
2023-04-02 20:36:27 +0300

    feature: setup location pwd as prompt on console

--- interfaces/app.js
+++ interfaces/app.js
@@ -6,11 +6,13 @@ const { APP_NAME, TERMINAL_HEIGHT } = require('./constants')
 const useHistory = require('./hooks/useHistory')
 const useInput = require('./hooks/useInput')
 const useTheme = require('./hooks/useTheme')
+const usePrompt = require('./hooks/usePrompt')
 
 const App = () => {
   const { history, groupHistory } = useHistory()
   const { groupInput } = useInput()
   const { theme } = useTheme()
+  const prompt = usePrompt()
 
   const handleInput = (input) => {
     groupInput(input)
@@ -18,7 +20,13 @@ const App = () => {
   }
 
   return (
-    <TerminalUI name={APP_NAME} colorMode={theme} onInput={handleInput} height={TERMINAL_HEIGHT}>
+    <TerminalUI
+      name={APP_NAME}
+      colorMode={theme}
+      onInput={handleInput}
+      height={TERMINAL_HEIGHT}
+      prompt={prompt}
+    >
       {history}
     </TerminalUI>
   )
