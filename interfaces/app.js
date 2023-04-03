5a49f7373be6d81d0027b48fe94efbdb81de983b
Ando RAMANAMIHANTA <iamando@tuta.io>
2023-04-03 11:24:11 +0300

    feature: use loader on process not ready to avoid error on use

--- interfaces/app.js
+++ interfaces/app.js
@@ -1,23 +1,29 @@
 const React = require('react')
 const { default: TerminalUI } = require('react-terminal-ui')
 
+const Loader = require('./components/loader')
+
 const { APP_NAME, TERMINAL_HEIGHT } = require('./constants')
 
 const useHistory = require('./hooks/useHistory')
 const useInput = require('./hooks/useInput')
 const useTheme = require('./hooks/useTheme')
 const usePrompt = require('./hooks/usePrompt')
+const useHealth = require('./hooks/useHealth')
 
 const App = () => {
   const { history, groupHistory } = useHistory()
   const { groupInput } = useInput()
   const { theme } = useTheme()
   const prompt = usePrompt()
+  const isReady = useHealth()
 
   const handleInput = (input) => {
     groupInput(input)
     groupHistory(input)
   }
+
+  if (!isReady) return <Loader />
 
   return (
     <TerminalUI
